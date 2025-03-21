de23241701b8432025529eaa4ccac34ea7c74614
Raven <iptime0817@naver.com>
2025-03-21 14:45:35 +0900

    오후 전반 수업진행

--- 03customer.py
+++ 03customer.py
@@ -4,9 +4,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sb
 import sklearn as skl
-import matplotlib as mpl # 그래프
-import matplotlib.pyplot as plt # 그래프 관련
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sb
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from matplotlib import font_manager
 from matplotlib import rc
@@ -60,10 +62,49 @@ print('- '*40)
 # 스케일링
 STS = StandardScaler()
 df = pd.DataFrame(STS.fit_transform(df),columns=df.columns)
-print('스케일링')
 print(df)
 
 kmeans = KMeans(n_clusters=4,random_state=0)
 cluster = kmeans.fit(df)
+df['cluster'] = cluster.labels_
+df.rename(columns={'mean':'월평균','median':'중앙값','max':'최대값','min':'최소값','membership_period':'회원기간'},inplace=True)
 
+call(df.head())
+print('- '*40)
+
+print(df.groupby('cluster').count())
+print('- '*40)
+'''
+          월평균   중앙값   최대값   최소값  회원기간
+cluster
+0        1334  1334  1334  1334  1334
+1         771   771   771   771   771
+2        1249  1249  1249  1249  1249
+3         838   838   838   838   838
+'''
+print('TEST')
+# 시각화
+X = df.copy()
+X.drop('cluster',axis=1,inplace=True)
+pca = PCA(n_components=2)
+pca.fit(X)
+pca_X = pca.transform(X)
+pca_df = pd.DataFrame(pca_X)
+pca_df['cluster'] = df['cluster']
+print(pca_df)
+print('- '*40)
+
+fig,ax = plt.subplots(1,2,figsize=(12,5))
+sb.scatterplot(pca_df,x=0,y=1,hue='cluster',palette='Set1',ax=ax[0])
+
+
+for i in (pca_df ["cluster"].unique()):
+    tmp = pca_df.loc[pca_df["cluster"] == i]
+    plt.scatter(tmp[0], tmp[1])
+plt.legend(sorted(pca_df['cluster'].unique()))
+plt.show()
+
+dfo = pd.read_csv(pathjoin)
+df_join = pd.concat([df,dfo],axis=1)
+print(df_join)
 print('='*80)
